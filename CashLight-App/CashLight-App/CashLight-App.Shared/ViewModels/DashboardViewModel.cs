63131b73f61f920578fe7ddb1f8171fb1707e338
Jan-Paul Kleemans <jpkleemans@gmail.com>
2014-12-09 11:24:03 +0100

    DesignTime data for DashboardView

--- CashLight-App/CashLight-App/CashLight-App.Shared/ViewModels/DashboardViewModel.cs
+++ CashLight-App/CashLight-App/CashLight-App.Shared/ViewModels/DashboardViewModel.cs
@@ -26,12 +26,42 @@ namespace CashLight_App.ViewModels
 
         public void InitTransactions()
         {
-            List<Transaction> all = TransactionModel.GetAll();
-            List<Transaction> mostImportantIncomes = TransactionModel.getMostImportantTransactionsBij(all, new DateTime(2014, 10, 01), new DateTime(2014, 11, 01));
-
-            foreach (Transaction item in mostImportantIncomes)
+            if (IsInDesignMode)
             {
-                ImportantIncomes.Add(item);
+                ImportantIncomes.Add(new Transaction()
+                {
+                    Datum = new DateTime(2014, 10, 01),
+                    Naam = "Test",
+                    Bedrag = 100
+                });
+                ImportantIncomes.Add(new Transaction()
+                {
+                    Datum = new DateTime(2014, 10, 05),
+                    Naam = "Test2",
+                    Bedrag = 350
+                });
+                ImportantIncomes.Add(new Transaction()
+                {
+                    Datum = new DateTime(2014, 10, 11),
+                    Naam = "Test3",
+                    Bedrag = 149.99
+                });
+                ImportantIncomes.Add(new Transaction()
+                {
+                    Datum = new DateTime(2014, 10, 28),
+                    Naam = "Test4",
+                    Bedrag = 199
+                });
+            }
+            else
+            {
+                List<Transaction> all = TransactionModel.GetAll();
+                List<Transaction> mostImportantIncomes = TransactionModel.getMostImportantTransactionsBij(all, new DateTime(2014, 10, 01), new DateTime(2014, 11, 01));
+
+                foreach (Transaction item in mostImportantIncomes)
+                {
+                    ImportantIncomes.Add(item);
+                }
             }
         }
     }
